39ba14ddbf779154b39b90b3511e0452c881cbb5
robert-virtual <robertocastill63@gmail.com>
2022-03-07 19:19:25 -0600

    estylos de correo📧

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -7,15 +7,32 @@ exports.genPin = () => {
 
 exports.getHtml = (code) => {
   return `
-    <header>
-        <p>Codigo de Recuperacion</p>
-        <p>Ferreteria Movil</p>
-    </header>
-    <section>
-        <p>Copia el pin e ingresalo en la aplicacion</p>
-        <h1>${code}</h1>
-    </section>
-    `;
+        <html style="font-family: sans-serif">
+        <body style="padding: 0; margin: 0">
+            <main>
+            <header style="background-color: #10b981; padding: 0.5rem; color: white">
+                <p style="font-size: 1.5rem">Codigo de Recuperacion</p>
+                <p>Ferreteria Movil</p>
+            </header>
+            <section style="padding: 0.5rem; display: flex; flex-direction: column">
+                <h1
+                style="
+                    color: #10b981;
+                    font-size: 2rem;
+                    letter-spacing: 0.5rem;
+                    font-weight: bold;
+                "
+                >
+                ${code}
+                </h1>
+                <p style="font-size: 1.25rem; color: gray">
+                Copia el pin e ingresalo en la aplicacion
+                </p>
+            </section>
+            </main>
+        </body>
+        </html>
+        `;
 };
 
 exports.transporter = nodemailer.createTransport({
