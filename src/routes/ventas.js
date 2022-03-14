c3054d85c93fbcd219b99c1dca394cc6c4135dfe
robert-virtual <robertocastill63@gmail.com>
2022-03-14 13:51:55 -0600

    ventas📀

---
+++ src/routes/ventas.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+
+// crear
+router.post("/");
+
+// obtener rango
+router.get("/");
+
+// obtener compras de usuario
+router.get("/");
+
+module.exports = router;
