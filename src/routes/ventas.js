2a7fd2dd947700f2724beaae0721113e4e255e44
robert-virtual <robertocastill63@gmail.com>
2022-03-16 22:06:45 -0600

    auth ventas

--- src/routes/ventas.js
+++ src/routes/ventas.js
@@ -1,18 +1,26 @@
-
 const { query } = require("express-validator");
-const { listarVentas, listarVentasUsuario, crearVenta } = require("../controllers/ventas");
+const {
+  listarVentas,
+  listarVentasUsuario,
+  crearVenta,
+} = require("../controllers/ventas");
 const { auth } = require("../middlewares/auth");
 
 const router = require("express").Router();
 
 // crear
-router.post("/", crearVenta );
+router.post("/", auth, crearVenta);
 
 // obtener rango
-router.get("/listar",auth,query("inicio").isInt().default(0),query("cantidad").isInt().default(20) , listarVentas);
-
+router.get(
+  "/listar",
+  auth,
+  query("inicio").isInt().default(0),
+  query("cantidad").isInt().default(20),
+  listarVentas
+);
 
 // obtener compras de usuario
-router.get("/",auth, listarVentasUsuario);
+router.get("/", auth, listarVentasUsuario);
 
 module.exports = router;
